a08760374c65275f52fc6e366daac8f4ed1da832
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-06-18 15:41:59 +0700

    feat: setup logging to file

--- belajar-nodejs-restful-api/src/app/logging.js
+++ belajar-nodejs-restful-api/src/app/logging.js
@@ -1,9 +1,32 @@
 import winston from "winston";
+import * as fs from "node:fs";
+
+
+// try {
+//   const dirname = "./src/logs";
+  
+//   if (!fs.existsSync(dirname)) {
+//     fs.mkdirSync(dirname);
+//   }
+// } catch (err) {
+//   console.error(err);
+// }
 
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  transports: [new winston.transports.Console({})],
+  transports: [
+    new winston.transports.Console({}),
+    new winston.transports.File({
+      filename: "infos.log",
+      dirname : "./src/logs"
+    }),
+    new winston.transports.File({
+      level: "error",
+      filename: "errors.log",
+      dirname : "./src/logs"
+    }),
+  ],
 });
 
 export { logger };
